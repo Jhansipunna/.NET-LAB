a9cdb8f4db97cd4407cf95c3723b9219d2854c93
Jhansipunna <69448522+Jhansipunna@users.noreply.github.com>
2021-03-19 16:37:06 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace ExceptionEx
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a number:");
+                var num = int.Parse(Console.ReadLine());
+                Console.WriteLine($"square of(num)is {num * num}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error info:"+ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("re-try with adifferent number");
+            }
+        }
+        
+    }
+}
